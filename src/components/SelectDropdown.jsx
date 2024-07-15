163ac8fe46f6b20a25046c572fd0d5cde1954414
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-07-15 16:38:29 -0700

    dropdown question

---
+++ src/components/SelectDropdown.jsx
@@ -0,0 +1,49 @@
+import React, {useState,useEffect} from 'react';
+
+const countries = [
+    {
+        name: 'India', value: 'IN', cities: [
+            'Delhi', 'Mumbai'
+        ]
+    },
+    {
+        name: 'Pak', value: 'PK', cities: [
+            'Lahore', 'Karachi'
+        ]
+    },
+    {
+        name: 'Bangladesh', value: 'BG', cities: [
+            'Dhaka', 'Chittagong'
+        ]
+    },
+]
+
+const SelectDropdown = () => {
+    const [country, setCountry] = useState([]);
+
+  useEffect(() => {
+    console.log(country[0]);
+    console.log(country[1]);
+  }, [country]);
+
+  return (
+    <div className="App">
+      <div>
+        <label>Country:</label>
+        <select value={country} onChange={(e) =>{console.log(e.target.value); setCountry(e.target.value)}}>
+            {countries.map((country, index) => {
+                return <option value={index}>{country.name}</option>
+            })}
+        </select>
+        <label>City:</label>
+        <select>
+            {countries[country].cities.map((city, index) => {
+                return <option value={index}>{city}</option>
+            })}
+        </select>
+      </div>
+    </div>
+    )
+}
+
+export default SelectDropdown;
